aabc05daf8028888eb64c52ee6beb2505794198e
iameo <okwudike.emmanuel@gmail.com>
2020-04-07 15:48:32 +0100

    abeg

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn first:server
+web: gunicorn app:server
 
--- app.py
+++ app.py
@@ -288,6 +288,6 @@ server = app.server
 
 
 if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", debug=False)
+    app.run_server(debug=False)
 
 
